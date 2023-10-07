357ded749f03bfeb0885cdca827628e072f0d7f3
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:14:38 -0300

    Comentários de código e Docstrings

---
+++ Python/Logica_Basica/aula1_comentarios.py
@@ -0,0 +1,14 @@
+# Aula 1 - Comentarios de codigo e Docstrings como comentario
+
+'''
+DocString, não é comentario, o programa le 
+oque esta dentro das aspas
+
+'''
+"""
+Usar para escrever suas notas, ou explicações de codigos
+
+"""
+# Permite fazer um comentario, não exibe oque está a fentre da cerquilha
+
+print (2+2) # print é imprimir, mostrar na tela
