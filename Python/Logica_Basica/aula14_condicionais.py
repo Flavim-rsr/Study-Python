4764b0b6267405861719e3bcb8f00d14d200a0f2
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:23:12 -0300

    Blocos de código + if / elif / else (condicionais)

---
+++ Python/Logica_Basica/aula14_condicionais.py
@@ -0,0 +1,34 @@
+# Aula 14 - Introdução aos blocos de código + if / elif / else (condicionais)
+
+# if / elif      / else 
+# se / se não se  / se não
+
+condicao1 = False   
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+# Depois da preimeira condição true o programa sai do bloco e executa oque está fora
+
+if condicao1:
+    print("Código para condição 1")
+    print("Código para condição 1")
+elif condicao2:
+    print("Código para condição 2")
+elif condicao3:
+    print("Código para condição 3")
+elif condicao4:
+    print("Código para condição 4")
+else:
+    print("")
+
+'''
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print("Você entrou no sistema")
+elif entrada == 'sair':
+    print("Você saiu do sistema")
+else:
+    print("Você não digitou nem entrar nem sair")
+'''
