9c5935bb16bf946929ff646ce3827d81c857ff90
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:20:47 -0300

    Introdução aos operadores aritméticos (matemática)

---
+++ Python/Logica_Basica/aula8_operadores_aritmeticos.py
@@ -0,0 +1,25 @@
+# Aula 8 - introdução aos operadores aritméticos (matematica)
+
+adicao = 10 + 10
+print("Adição:", adicao)
+
+subtracao = 10 - 5
+print("Subtração:", subtracao)
+
+multiplicacao = 5 * 5
+print("Multiplicação:", multiplicacao)
+
+divisao = 10 / 2.2  # Respostas das divisões sempre será um ponto flutuante (float)
+print("Divisão:", divisao)
+
+divisao_inteira = 10 // 2.2 # Divisão inteira sempre vai arrendondar e cortar as casas decimais
+print("Divisão inteira:", divisao_inteira)
+
+exponeciacao = 5 ** 10
+print("Exponenciação:", exponeciacao)
+
+modulo = 25 % 5 # Resto da divisão
+print("Modulo:", modulo)
+print(10 % 8 == 0)
+print(16 % 8 == 0) # para saber se um numero é divisivel o resto da divisão tem que ser zero
+print(10 % 2 == 0) # para o numero ser par ele tem que ter o resto zero
