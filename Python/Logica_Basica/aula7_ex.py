70d5813909a647b3bd59be9cce116f36d4a85747
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:20:14 -0300

    Ex: com variáveis e tipos de dados

---
+++ Python/Logica_Basica/aula7_ex.py
@@ -0,0 +1,15 @@
+# Aula 7 - Exercicio com variaveis e tipos de dados
+
+nome = "Flavio"
+sobrenome = "Rodrigo"
+idade = 21
+nascimento = 2023 - idade
+maior_idade = idade >= 18
+altura = 1.70
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de nascimento: ", nascimento)
+print("É maior de idade?: ", maior_idade)
+print("Altura em metros: ", altura)
