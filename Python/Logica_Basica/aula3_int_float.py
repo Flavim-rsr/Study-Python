e6137a0e1294703eb77524e7d441a01ddc0b47b6
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:17:21 -0300

    Tipos int e float

---
+++ Python/Logica_Basica/aula3_int_float.py
@@ -0,0 +1,25 @@
+# Aula 3 - Tipo int e float (numeros) - introdução aos tipos de dados
+
+'''
+Tipo int
+int -> Número inteiro
+O tipo int representa qualquer numero positivo ou negativo 
+int sem sinal é considerado positivo
+
+'''
+print(11)   # int
+print(11)   # int
+
+'''
+float -> Número com ponto flutuante
+O tipo float representa qualquer número positivo ou negativo com um ponto flutuante
+float sem sinal é considerado positivo
+
+'''
+print(1.1, -1.1) # Usa-se ponto para separar as casas decimais 
+
+# A função type mostra o tipo que o Phyton inferiu o valor
+print(type('Flavio'))
+print(type(0))
+print(type(1.1))
+print(type(-1.1),type(1.0))
