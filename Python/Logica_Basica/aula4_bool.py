959d3161553d8528216d54534089f9658aabaa62
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:17:51 -0300

    Tipo bool

---
+++ Python/Logica_Basica/aula4_bool.py
@@ -0,0 +1,13 @@
+# Aula 4 - Tipo bool (boolean) - introdução aos tipos de dados
+
+'''
+Tipo de dado bool (boolean)
+Ao questionar algo em um programa, só existem duas respostas possiveis:
+sim (true) ou não (false)
+Existem varios operadores para "questionar": 
+Dentro eles o ==, que é um operador logico que questiona se um valor é igual ao outro
+
+'''
+print(10 == 10) # Sim => True (verdadeiro)
+print(10 == 11) # Não => False (falso)
+print(type(10 == 10)) #bool
