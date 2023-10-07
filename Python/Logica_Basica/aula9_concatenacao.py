a71cf9ed5200ad9895491c2e728217d780aba9c5
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-06 23:21:23 -0300

    Concatenação (+) e repetição (*)

---
+++ Python/Logica_Basica/aula9_concatenacao.py
@@ -0,0 +1,10 @@
+# Aula 9 - Concatenação (+) e repetição (*) com operadores aritméticos
+
+concatenacao = "A"+ " " + "B" + " " + "C"
+print(concatenacao)
+
+a_dez_vezes = "A" * 10
+tres_vezes_flavio = "Flavio" * 3
+print(a_dez_vezes)
+print(tres_vezes_flavio)
+print("1"+str(2)) # Concatenar numeros e strings resulta na junção dos dois e não na soma
