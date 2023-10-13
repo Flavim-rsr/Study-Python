b5473e690331f478777c8d7291005956e827316c
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-12 22:06:03 -0300

    range + for para usar intervalos de números

---
+++ Python/Logica_Basica/aula36_for_range.py
@@ -0,0 +1,16 @@
+# Aula 36 - range + for para usar intervalos de números
+
+'''
+For + range
+range -> range (start, stop, step)
+'''
+# Start = qual número inicia
+# Stop = qual numero para
+# Step = de quantos em quantos números a sequencia vai
+
+numeros = range(0, 100, 2)
+
+for numero in numeros:
+    print(numero)
+
+
